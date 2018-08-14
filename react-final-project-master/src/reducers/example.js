ede63b0ddc0ef95e39c56c02ad57d8fb0f792edf
unknown <=>
2018-08-13 21:55:03 -0300

    first commit

---
+++ react-final-project-master/src/reducers/example.js
@@ -0,0 +1,13 @@
+const initialState = {
+  user: null,
+  users: [],
+}
+
+const example = ( state = initialState, action ) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
+
+export default example;
--- react-final-project-master/src/routes/index.js
+++ react-final-project-master/src/routes/index.js
@@ -3,16 +3,20 @@ import { Route, Switch } from 'react-router-dom';
 import MainView from './mainView/mainView';
 import HomeView from './homeView/homeView';
 import AboutView from './aboutView/aboutView';
+import { Provider } from 'react-redux';
+import store from '../store/store'
 
 const getRoutes = function() {
     return (
-        <div>
-            <Route name="Main" component={MainView} />
-            <Switch>
-                <Route exact path="/" component={HomeView} />
-                <Route path="/about" component={AboutView} />
-            </Switch>
-        </div>
+        <Provider store={store}>
+            <div>
+                <Route name="Main" component={MainView} />
+                <Switch>
+                    <Route exact path="/" component={HomeView} />
+                    <Route path="/about" component={AboutView} />
+                </Switch>
+            </div>
+        </Provider>
     )
 };
 
